cd368413a0a873eb20100a50ef5f1026eaf046e1
skorpion <jmosinski@o2.pl>
2023-02-14 21:09:31 +0100

    JS download items from html

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const error = document.querySelector('.error')
+
+const model = document.querySelector('#model')
+const year = document.querySelector('#year')
+const mileage = document.querySelector('#mileage')
+const visit = document.querySelector('#visit')
+const accident = document.querySelector('#accident')
+
+const btnAdvice = document.querySelector('.btns__advice')
+const btnClear = document.querySelector('.btns__clear')
+
+const spamBrand = document.querySelector('.spambrand')
+const spamYear = document.querySelector('.spamyear')
+const spamMileage = document.querySelector('.spammileage')
+const spamVisit = document.querySelector('.spamvisit')
+const spamAccident = document.querySelector('.spamaccident')
+
+const info = document.querySelector('.advice__sell--info')
+
+console.log(info)
