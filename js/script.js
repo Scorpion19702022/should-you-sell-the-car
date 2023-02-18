4b603e20135f95ca4b953fd7b6acbad506a4f732
skorpion <jmosinski@o2.pl>
2023-02-19 00:23:23 +0100

    midify JS

--- js/script.js
+++ js/script.js
@@ -50,8 +50,6 @@ class CarInfo {
 				error.classList.add('actionerror')
 				info.textContent = 'wypełnij poprawnie wszystkie pola by uzyskać informacje'
 				spamAge.textContent = '0'
-			} else {
-				return true
 			}
 
 			if (year.value > todayYear) {
@@ -220,11 +218,6 @@ class CarInfo {
 				accident.value = 0
 				spamAge.textContent = age
 				error.classList.remove('actionerror')
-				// } else {
-				// 	error.classList.add('actionerror')
-				// 	info.textContent = 'wypełnij poprawnie wszystkie pola by uzyskać informacje'
-				// 	spamAge.textContent = '0'
-				// 	infoFinal.textContent = ''
 			}
 		}
 		show1()
